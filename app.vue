72b42d6c1656c4c54365fa8091d0319ede2723bd
Steven Lin <boss00613333@gmail.com>
2023-09-14 12:56:21 +0800

    no message

--- app.vue
+++ app.vue
@@ -11,8 +11,9 @@ const { locale, locales } = useI18n()
 // 設定page meta
 useMetaHead({
     title: 'DigiSalad Nuxt3 基本使用',
-    description: 'DigiSalad Nuxt3 基本使用'
+    description: 'DigiSalad Nuxt3 基本使用',
 })
+
 onMounted(() => {
     toggleTransitionComplete(true)
 })
