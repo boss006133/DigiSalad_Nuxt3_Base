68b729663749c22470e4ad3aa13e28ea0fd66340
DESKTOP-9GH44K3\User <boss00613333@gmail.com>
2023-11-13 10:12:08 +0800

    將/ds-guide 加入 robots.txt disallow

--- vender/robots/config.ts
+++ vender/robots/config.ts
@@ -1,12 +1,12 @@
 //https://nuxtseo.com/robots/guides/nuxt-config
 const isProduction = process.env.NUXT_PUBLIC_NODE_ENV === 'production'
 const pathAll = isProduction ? '' : '/'
-const disallow = [] as Array<string>
+let disallow = [] as Array<string>
 pathAll && disallow.push(pathAll)
 
 //新增額外需要disallow paths
 const extraDisallowPaths = ['/ds-guide']
-disallow.concat(extraDisallowPaths)
+disallow = disallow.concat(extraDisallowPaths)
 
 export default {
     // provide simple disallow rules for all robots `user-agent: *`
